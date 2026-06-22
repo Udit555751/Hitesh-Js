b54ef2d11fd5a43fa3486c466092f2153d1ea426
Udit Tiwari <125749380+Udit555751@users.noreply.github.com>
2026-06-22 16:33:44 +0000

    truthy and falsy value

--- 04_control_flow/03_truthy_falsy.js
+++ 04_control_flow/03_truthy_falsy.js
@@ -23,3 +23,54 @@ let obj = {};  // object always give the array of keys or array of values, so we
 if(Object.keys(obj).length === 0 ){
     console.log("Object is Empty");
 }
+
+
+// Nullish coalesing Operator (??) : null undefined ==> 
+
+// Ye left side ki value return karta hai agar wo null ya undefined na ho, 
+// Dhyan rahe, ?? sirf null aur undefined ko check karta hai. 0, false, aur "" ko valid values maanta hai.
+
+// ?? (Nullish Coalescing Operator) check karta hai ki left side ki value null ya undefined hai ya nahi.
+// Agar left side null ya undefined ho ➝ right side ki value return hoti hai.
+// Warna left side ki value return hoti hai.
+
+
+let val1;
+val1 = 7 ?? 3;
+console.log(val1);
+
+const val2 = null ?? 2;
+console.log(val2);
+
+let val3 = undefined ?? 4;
+console.log(val3);
+
+const val4 = null ?? 3 ?? 5;  // null ke baad jo first value milegi wahi return kr dega.
+console.log(val4);
+
+// Difference: ====>
+
+// || → saari falsy values (0, false, "", null, undefined, NaN) ko reject karta hai.
+// ?? → sirf null aur undefined ko reject karta hai.  
+
+// Example ==>
+
+console.log(0 || 5);   // 5
+console.log(0 ?? 5);   // 0
+
+console.log("" || "Hi");  // Hi
+console.log("" ?? "Hi");  // ""
+
+console.log(null || 5);   // 5
+console.log(null ?? 5);   // 5
+
+
+
+
+// ========= Turnery Opertor (?) ========>
+
+// condition ? true : false
+
+const iceTeaPrice = 100;
+
+iceTeaPrice <= 80 ? console.log('Less than 80') : console.log('More than 80');
