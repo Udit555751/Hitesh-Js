9e91791aef33af7eda53baf77b857c1e6d7769b1
Udit Tiwari <125749380+Udit555751@users.noreply.github.com>
2026-06-22 14:00:35 +0000

    switch case

---
+++ 04_control_flow/02_switch.js
@@ -0,0 +1,47 @@
+// Switch Case ===>
+
+// Switch Statement ===> Mostly it will use to check multiple conditions and if we don't use break statement then 
+// when it will match consition after below that condition all statement will execute except default case.
+
+// Syntax -->
+
+// switch(key){
+//     case value:
+//         break;
+    
+//     default:
+//         break;   
+// }
+
+
+const month = 3;
+
+switch(month){
+    case 1: 
+        console.log('Jan');
+        break;
+    
+    case 2: 
+        console.log("Feb");
+        break;
+    
+    case 3:
+        console.log("March");
+        break;
+    
+    case 4:
+        console.log("April");
+        break;
+        
+    case 5:
+        console.log("May");
+        break;
+
+    case 6:
+        console.log("June");
+        break;
+       
+    default:
+        console.log("Default Case Match");
+        break;    
+}
