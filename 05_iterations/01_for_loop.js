4d46d5870fe1bba5e2aaa9b8c248c618ca6128af
Udit Tiwari <125749380+Udit555751@users.noreply.github.com>
2026-06-23 13:49:41 +0000

    for loop

--- 05_iterations/01_for_loop.js
+++ 05_iterations/01_for_loop.js
@@ -34,5 +34,38 @@ for (let i = 5; i <= 5; i++) {
 // Or 
 
 for(let i = 1; i <= 10; i++){
-    console.log(`5 * ${i} = ${5 * i}`);
+    // console.log(`5 * ${i} = ${5 * i}`);
+}
+
+
+// for loop on array ===>
+
+const myArr = ['mango', 'apple', 'banana'];
+
+for(let i = 0; i < myArr.length; i++){
+    const element = myArr[i];
+    // console.log(element);
+}
+
+
+// break and continue keyword ===> In break, after matching the condition control comes outside of loop and in continue after 
+// matching the condition once it will ignore and execution will continue.
+
+for(let i = 0; i <=10; i++){
+    
+    if(i == 5){
+        console.log(`Dedected 5`);
+        break;
+    }
+
+    console.log(`the value is ${i}`);
+}
+
+
+for(let j = 0; j <= 10; j++){
+    if(j == 5){
+        console.log(`Dedected 5`);
+        continue;
+    }
+    console.log(`the value is ${j}`);
 }
